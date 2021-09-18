6438fd87978b1b40db366eb77d1575efbd6bcf67
Shoma <sho1228.k@gmail.com>
2021-09-19 02:35:28 +0900

    [create] setting.vueの作成

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -71,7 +71,7 @@ import Vue from 'vue'
 import AppHeader from '../components/AppHeader.vue'
 import FriendItem from '../components/FriendItem.vue'
 export default Vue.extend({
-  name: 'index',
+  name: 'Index',
   components: { FriendItem, AppHeader },
   data() {
     return {
---
+++ app/pages/setting.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <app-header />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import AppHeader from '../components/AppHeader.vue'
+export default Vue.extend({
+  components: { AppHeader },
+})
+</script>
+
+<style scoped></style>
