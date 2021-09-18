8f1310cb03aab925d4c2bda5d6d32fa371773d1a
Shoma <sho1228.k@gmail.com>
2021-09-19 01:50:16 +0900

    [style] checkboxの実装

--- app/components/FriendItem.vue
+++ app/components/FriendItem.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="friend">
-    <img
-      src="../assets/icon_sample.png"
-      alt="ユーザーアイコン"
-      class="friend-icon"
-    />
-    <p class="friend-name ma-0 ml-4">Shoma</p>
+  <div class="friend" @click="isChecked = !isChecked">
+    <div class="friend-inner">
+      <img
+        src="../assets/icon_sample.png"
+        alt="ユーザーアイコン"
+        class="friend-inner-icon"
+      />
+      <p class="friend-inner-name ma-0 ml-4">Shoma</p>
+    </div>
+    <div :class="['checkbox', isChecked ? 'checked' : '']">
+      <v-icon size="16" class="checkbox-icon">mdi-star</v-icon>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-  name: 'friend-item',
+  name: 'FriendItem',
+  data() {
+    return {
+      isChecked: false,
+    }
+  },
 })
 </script>
 
@@ -20,14 +30,38 @@ export default Vue.extend({
 .friend {
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  &-inner {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    &-icon {
+      width: 36px;
+      height: 36px;
+      border-radius: 50%;
+    }
+    &-name {
+      font-size: 18px;
+      color: #08072f;
+    }
+  }
+}
+
+.checkbox {
+  display: flex;
+  width: 24px;
+  height: 24px;
+  background-color: gray;
+  border-radius: 50%;
   &-icon {
-    width: 36px;
-    height: 36px;
-    border-radius: 50%;
+    flex: 1;
+    color: white;
+    text-align: center;
   }
-  &-name {
-    font-size: 18px;
-    color: #08072f;
-  }
+}
+
+.checked {
+  background-color: unset;
+  background-image: $primary-gradient;
 }
 </style>
