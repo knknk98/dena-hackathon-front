571672843c0331a7d0fc1aa8b757fbf817f949fc
knknk98 <kayooshino@gmail.com>
2021-09-19 11:33:16 +0900

    ログイン画面UI

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>
-    <v-btn class="button" nuxt to="/signup/home">
-      <v-icon left> mdi-twitter </v-icon>Twitterでログイン
-    </v-btn>
-  </div>
+  <v-row class="mt-16">
+    <v-col class="text-center">
+      <img src="../assets/logo.png" alt="Logo" class="mb-5" width="150px" />
+      <blockquote class="blockquote">
+        <v-btn class="button" nuxt to="/signup/home">
+          <v-icon left> mdi-twitter </v-icon>Twitterでログイン
+        </v-btn>
+      </blockquote>
+    </v-col>
+  </v-row>
 </template>
 
 <style lang="scss" scoped>
